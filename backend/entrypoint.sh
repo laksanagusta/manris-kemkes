687cc5e496e0596fe57540e4a2ea38e0fae7434e
Dika Laksana <dikalaksana@MacBook-Air-Dika.local>
2026-04-20 01:15:50 +0700

    disable migrations

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Running database migrations..."
-/app/migrate -path /app/migrations -database "$DATABASE_URL" up
+# echo "Running database migrations..."
+# /app/migrate -path /app/migrations -database "$DATABASE_URL" up
 
 echo "Starting server..."
 exec /app/server
